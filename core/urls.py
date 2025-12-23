af20495dd5c3f09fd72dca54069db253885569db
huyhandsome6996 <iamdoinb6996@gmail.com>
2025-12-23 12:46:59 +0700

    api cho booking

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import CarePartnerListAPIView, CarePartnerDetailAPIView
+from .views import CarePartnerListAPIView, CarePartnerDetailAPIView, BookingCreateAPIView
 
 urlpatterns = [
     path('care-partners/', CarePartnerListAPIView.as_view()),
     path('care-partners/<int:pk>/', CarePartnerDetailAPIView.as_view()),
+    path('bookings/', BookingCreateAPIView.as_view()),
 ]
